9c4ce701c32ab9f815a2d59b54834bcd2da61051
Sanger Steel <sangersteel@Sangers-MacBook-Air.local>
2022-01-27 21:52:39 -0500

    got initial score

---
+++ data_preprocessing/lemmatizing.py
@@ -0,0 +1,78 @@
+import nltk
+from nltk.tag.stanford import StanfordPOSTagger
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet as wn
+
+
+lemmatizer = WordNetLemmatizer()
+
+_path_to_model = 'files/stanford-postagger/models/english-bidirectional-distsim.tagger'
+_path_to_jar = 'files/stanford-postagger/stanford-postagger.jar'
+st = StanfordPOSTagger(model_filename=_path_to_model, path_to_jar=_path_to_jar, java_options = '-Xmx4G')
+
+
+mappings = {}
+mappings['JJ'] = 'a'
+mappings['JJR'] = 'a'
+mappings['JJS'] = 'a'
+mappings['NN'] = 'n'
+mappings['NNS'] = 'n'
+mappings['NNP'] = 'n'
+mappings['NNPS'] = 'n'
+mappings['RB'] = 'r'
+mappings['RBR'] = 'r'
+mappings['RBS'] = 'r'
+mappings['VB'] = 'v'
+mappings['VBD'] = 'v'
+mappings['VBG'] = 'v'
+mappings['VBN'] = 'v'
+mappings['VBP'] = 'v'
+mappings['VBZ'] = 'v'
+
+def prevent_memory_bloat(tokens):
+    # This isn't a great workaround as its eliminating information for the sake of memory. Try and look in to either
+    # increasing available java memory or something else
+    # It also stops in the middle of a comment
+    sensor = tokens.split(" ")
+    print('len tokens:', len(tokens))
+    if len(set(sensor)) < 3 and len(tokens) > 500:
+        return " ".join(sensor[:15])
+
+    elif len(tokens) > 1000:
+        print('too many tokens for memory')
+        return tokens[:800]
+    #elif len(tokens) > 500:
+    #    print('spam flagged. Reducing size for memory')
+    #    return tokens[:250]
+    else:
+        return tokens
+
+
+
+def POS_tag(tokens):
+    #tokens = prevent_memory_bloat(tokens)
+    #print('tokens: ', tokens)
+    token_list = tokens.split()
+    try:
+        return st.tag(sorted(set(token_list), key = token_list.index))
+    except OSError:
+        print('resorting to nltk pos tagging.')
+        return nltk.pos_tag(sorted(set(token_list), key = token_list.index))
+
+
+
+def get_proper_lemma(word, POS):
+    wn.ensure_loaded()
+    try:
+        mappings[POS]
+        return lemmatizer.lemmatize(word, pos = mappings[POS])
+    except KeyError:
+        return lemmatizer.lemmatize(word)
+
+def lemmatize_doc(doc):
+    tags = POS_tag(doc)
+    #print('tags to lemmatize: ', tags)
+    lemmatized_doc = ""
+    for tuples in tags:
+        lemmatized_doc += get_proper_lemma(tuples[0], tuples[1]) + " "
+    return lemmatized_doc[:-1]
---
+++ data_preprocessing/preprocess_text.py
@@ -0,0 +1,51 @@
+from data_preprocessing.lemmatizing import lemmatize_doc
+import pandas as pd
+import string
+import nltk
+from nltk.corpus import stopwords
+import numpy as np
+nltk.download('omw-1.4')
+nltk.download('stopwords')
+symbols = list(string.punctuation + "®" + "\t" + "\n" + "\r")
+stops = stopwords.words('english')
+full_stops = stops + symbols
+
+print('symbols: ', symbols)
+
+def strip_symbols(doc): 
+    tokens = doc.split(" ")
+    new_tokens = []
+    for token in tokens:
+        if token != "":
+            new_token = token
+            for symbol in symbols:
+                if symbol in token:
+                    new_token = new_token.replace(symbol," ")
+            new_token = new_token.split(" ")
+            new_tokens.append(new_token)
+    tokens = flatten_irregular_list(new_tokens)
+    return tokens
+
+def flatten_irregular_list(li):
+    returnArray = []
+    for vals in li:
+        if isinstance(vals, list):
+            for val in vals:
+                returnArray.append(val)
+        else:
+            returnArray.append(vals)
+    return returnArray
+
+
+def pre_process_text(doc, lemmatizer = False):
+    doc = doc.lower()
+    tokens = strip_symbols(doc)
+    output = ""
+    for token in tokens:
+        if token not in full_stops and token.isalpha():
+                output += token.lower() + " "
+    if lemmatizer == True:
+        return lemmatize_doc(output[:-1])
+    else: 
+        return output[:-1]
+    
